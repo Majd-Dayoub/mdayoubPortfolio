df74386be6e46f929a02e091d209a7b5e6b82d4c
Majd-Dayoub <majd.dayoub04@gmail.com>
2024-12-16 17:33:54 -0500

    1) Added Navbar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import './index.css'
+import "./index.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Tech from "./components/Tech";
+import Projects from "./components/Projects";
 
 function App() {
-  
+  return (
+    <>
+      {/*Background snippet*/}
+      <div className="fixed -z-10 min-h-screen w-full [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
 
-  return (<>
+      <main className="flex flex-col items-center px-4 md:px-8 lg:px-16">
+        <Navbar />
+        <Hero />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,118 @@
-
+import { div } from "motion/react-client";
+import { useState } from "react";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { BiMenu, BiX } from "react-icons/bi";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default Navbar
+  const menuOpen = () => {
+    setIsOpen(!isOpen); // reverse state afte clicking menu iconn
+  };
+
+  return (
+    <nav className="fixed top-0 z-10 flex w-full items-center justify-between border-b border-b-gray-700 bg-black/70 py-6 text-white backdrop-blur-md md:justify-evenly">
+      <a
+        href="#home"
+        className="bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent opacity-80 text-3xl font-semibold transition-all duration-300 hover:opacity-100"
+      >
+        mdayoub
+      </a>
+
+      {/*Navbar List*/}
+      <ul className="hidden md:flex gap-10">
+        <a
+          href="#home"
+          className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100"
+        >
+          <li>Home</li>
+        </a>
+        <a
+          href="#tech"
+          className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100"
+        >
+          <li>Tech</li>
+        </a>
+        <a
+          href="#projects"
+          className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100"
+        >
+          <li>Projects</li>
+        </a>
+        <a
+          href="#contact"
+          className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100"
+        >
+          <li>Contact</li>
+        </a>
+      </ul>
+
+      {/*Contact List*/}
+      <ul className="hidden md:flex gap-5">
+        <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-blue-500">
+          {" "}
+          <BsLinkedin />
+        </li>
+        <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-orange-500">
+          {" "}
+          <BsGithub />
+        </li>
+      </ul>
+      {isOpen ? (
+        <BiX className="block md:hidden text-4xl" onClick={menuOpen} />
+      ) : (
+        <BiMenu className="block md:hidden text-4xl" onClick={menuOpen} />
+      )}
+
+      {isOpen && (
+        <div
+          className={`fixed right-0 top-[84px] flex h-screen w-1/2 flex-col items-start justify-start gap-10 border-l border-gray-800 bg-black/90 p-12 ${
+            isOpen ? "block" : "hidden"
+          }`}
+        >
+           {/*NavBar List*/}
+          <ul className="flex flex-col gap-8">
+            <a
+              href="#home"
+              className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100"
+            >
+              <li>Home</li>
+            </a>
+            <a
+              href="#tech"
+              className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100"
+            >
+              <li>Tech</li>
+            </a>
+            <a
+              href="#projects"
+              className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100"
+            >
+              <li>Projects</li>
+            </a>
+            <a
+              href="#contact"
+              className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100"
+            >
+              <li>Contact</li>
+            </a>
+          </ul>
+
+          {/*Contact List*/}
+          <ul className="flex flex-wrap gap-5">
+            <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-blue-500">
+              {" "}
+              <BsLinkedin />
+            </li>
+            <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-orange-500">
+              {" "}
+              <BsGithub />
+            </li>
+          </ul>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
